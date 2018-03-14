bb2f647e4c98c62b8178050efb4d55fb52166578
David Ramos <davidramos@unac.edu.co>
2018-03-13 22:54:09 -0500

    Delete home login

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../services/user.service';
-import { LoginUser } from '../../class_objects/user';
+
+
 import { Router } from '@angular/router';
 
 
@@ -11,21 +11,12 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-	constructor(private userService: UserService, private router: Router) { }
+	constructor() { }
 	is_login = localStorage.getItem('token') != null;
 	is_guest = localStorage.getItem('token') == null;
 
 	ngOnInit() {
 	}
 
-	login(value: any): void {
-		if (!value['user'] || !value['password']) { return; }
-		this.userService.logUser(value as LoginUser)
-		.subscribe(response => {
-			if(response['token'] != null) {
-				localStorage.setItem('token', response['token']);
-				this.router.navigate(['/products']);
-			}
-		});
-	}
+	
 }
