5de84787d6d217edabb0fb97a21cecfbafdc0828
unknown <magarnica@unac.edu.co>
2018-03-14 18:46:45 -0500

    cositas por guardar, por si algo

--- src/app/components/products/products.component.spec.ts
+++ src/app/components/products/products.component.spec.ts
@@ -1,3 +1,255 @@
+<div class="row"> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/nino">Productos Niños</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/office.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/mama">Productos Mamá</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/office.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/adulto" >Productos Adultos</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/pomada">Pomadas</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/pastilla">Pastillas</a> 
+      </div> 
+    </div> 
+  </div> 
+    <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/jarabe">Jarabes</a> 
+      </div> 
+    </div> 
+  </div> 
+ 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+ 
+  </div> 
+ 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+  </div> 
+</div>
+<div class="row"> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/nino">Productos Niños</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/office.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/mama">Productos Mamá</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/office.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/adulto" >Productos Adultos</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/pomada">Pomadas</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/pastilla">Pastillas</a> 
+      </div> 
+    </div> 
+  </div> 
+    <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/jarabe">Jarabes</a> 
+      </div> 
+    </div> 
+  </div> 
+ 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+ 
+  </div> 
+ 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+  </div> 
+</div>
+<div class="row"> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/nino">Productos Niños</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/office.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/mama">Productos Mamá</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/office.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/adulto" >Productos Adultos</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/pomada">Pomadas</a> 
+      </div> 
+    </div> 
+  </div> 
+  <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/pastilla">Pastillas</a> 
+      </div> 
+    </div> 
+  </div> 
+    <div class="col md4"> 
+    <div class="card" style="width: 18rem;"> 
+      <img class="card-img-top" src="../images/sample-1.jpg" alt="Card image cap"> 
+      <div class="card-body"> 
+        <!--<h5 class="card-title">Card title</h5>--> 
+        <!--<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>--> 
+        <a href="#" class="center" class="btn btn-primary" routerLink="/jarabe">Jarabes</a> 
+      </div> 
+    </div> 
+  </div> 
+ 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+ 
+  </div> 
+ 
+  <div class="col-md-4"> 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+ 
+  </div> 
+  <div class="col-md-4"> 
+ 
+  </div> 
+</div>
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ProductsComponent } from './products.component';
